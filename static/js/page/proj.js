b07f210464e4817ea0d096b12a738abe26301ba0
wjw <492533734@qq.com>
2018-08-10 13:05:18 +0800

    添加曲线图

---
+++ static/js/page/proj.js
@@ -0,0 +1,42 @@
+function showChart(el, data) {
+    var option = {
+        title: {
+            text: '折线图堆叠'
+        },
+        tooltip: {
+            trigger: 'axis'
+        },
+        legend: {
+            data: Object.keys(data)
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        toolbox: {
+            feature: {
+                saveAsImage: {}
+            }
+        },
+        xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: data[Object.keys(data)[0]].map((x, i) => 101 + i)
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series:
+            Object.keys(data).map(x => {
+                return {
+                    name: x,
+                    type: 'line',
+                    data: data[x]
+                }
+            })
+    };
+    return echarts.init(el).setOption(option);
+}
+
