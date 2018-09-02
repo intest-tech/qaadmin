29abd9d498844c65072ad607a531a524efc9c0df
ityoung <ityoung@foxmail.com>
2018-09-02 19:37:44 +0800

    update: use 0.0.0.0 as host;

--- manage.py
+++ manage.py
@@ -33,7 +33,7 @@ def project_with_id(pro_id):
 
 
 manager = Manager(app)
-manager.add_command('runserver', Server(use_debugger=True))
+manager.add_command('runserver', Server(use_debugger=True, host='0.0.0.0'))
 
 if __name__ == '__main__':
     manager.run()
