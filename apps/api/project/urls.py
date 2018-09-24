13e8d48ff4bc3452fb883d0efba67e48552bfbff
ityoung <ityoung@foxmail.com>
2018-09-24 17:12:46 +0800

    [WIP]update: clean up file and code; use base.html as template for index.html and job.html.

--- apps/api/project/urls.py
+++ apps/api/project/urls.py
@@ -1,10 +1,10 @@
 from .handlers import *
 
 url = [
-    ("/project/create", CreateProjectHandler),
+    # ("/project/create", CreateProjectHandler),
     # (r"/project/list", UpdateProjectHandler),
-    ("/project/delete", DeleteProjectHandler),
-    ("/project/gen-token", GenTokenHandler),
+    # ("/project/delete", DeleteProjectHandler),
+    # ("/project/gen-token", GenTokenHandler),
     ("/api/project", ListProjectHandler),
     ("/api/project/info", ProjectInfoHandler),
     ("/api/project/get-tags", ProjectTagsHandler),
--- manage.py
+++ manage.py
@@ -24,12 +24,18 @@ def favicon():
 
 @app.route('/project')
 def project():
-    return render_template('project-list.html')
+    return render_template('index.html')
 
 
 @app.route('/project/<pro_id>')
 def project_with_id(pro_id):
-    return render_template('project-with-id.html', id=pro_id)
+    return render_template('index.html', id=pro_id)
+
+
+@app.route('/project/<pro_id>/job/<job_id>')
+def job_with_id(pro_id, job_id):
+    print(pro_id, job_id)
+    return render_template('job.html', pro_id=pro_id, job_id=job_id)
 
 
 manager = Manager(app)
